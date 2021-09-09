e5bbe07e522f7d02f712b1ac2088fc6cbe207e02
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-09 17:16:10 -0400

    Add files via upload

---
+++ C#_Projects/Shipping/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome Message
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            // Gets item weight from user
+            Console.WriteLine("Please enter the package weight.");
+            decimal packageWeight = Convert.ToDecimal(Console.ReadLine());
+
+            // This checks if the item is too heavy
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Your package is too heavy!");
+            }
+            // If item isn't too heavy, user is prompted to enter dimensions
+            else
+            {
+                Console.WriteLine("Please enter the package width.");
+                decimal packageWidth = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Please enter the package height.");
+                decimal packageHeight = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Please enter the package length.");
+                decimal packageLength = Convert.ToDecimal(Console.ReadLine());
+
+                // Calculating dimensions and storing in var packageDimension for later use
+                decimal packageDimension = (packageWidth * packageHeight * packageLength);
+
+                // This checks if item is too big
+                if (packageWidth + packageHeight + packageLength > 50)
+                {
+                    Console.WriteLine("Your package is too big!");
+                }
+                // If item isn't too big, the cost is calculated and displayed to user
+                else
+                {
+                    decimal packageCost = (packageDimension * packageWeight) / 100;
+                    Console.WriteLine("The packaging cost is $" + packageCost);
+                }
+            }
+            // Ensures user can read result
+            Console.ReadLine();
+        }
+    }
+}
