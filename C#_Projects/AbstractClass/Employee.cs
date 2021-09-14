a13d4f1a761a2db9d7628e3148523545370444ca
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-14 17:53:09 -0400

    Add files via upload

---
+++ C#_Projects/AbstractClass/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Employee : Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FName + LName);
+            return;
+        }
+    }
+}
---
+++ C#_Projects/AbstractClass/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    abstract class Person
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+    }
+}
