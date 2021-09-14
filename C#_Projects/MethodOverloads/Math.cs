d9ead82ea740978b08ff1b789401392a4450cd2f
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-14 14:54:28 -0400

    Add files via upload

---
+++ C#_Projects/MethodOverloads/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloads
+{
+    class Math
+    {
+        public int Add(int input)
+        {
+            return input + 2;
+        }
+
+        public int Add(decimal input)
+        {
+            // Converts input to int
+            return Convert.ToInt32(input) - 2;
+        }
+
+        public int Add(string input)
+        {
+            // Converts input to an int
+            return Convert.ToInt32(input) * 2;
+        }
+    }
+}
---
+++ C#_Projects/MethodOverloads/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // New instance
+            Math math = new Math();
+            // Asks for input
+            Console.WriteLine("Enter a number");
+            // Stores input
+            int input = Convert.ToInt32(Console.ReadLine());
+            // Display result
+            Console.WriteLine(math.Add(input));
+
+            // New instance
+            Math math1 = new Math();
+            // Asks for input
+            Console.WriteLine("Enter a decimal");
+            // Stores input
+            decimal input1 = Convert.ToDecimal(Console.ReadLine());
+            // Display result
+            Console.WriteLine(math1.Add(input1));
+
+            // New instance
+            Math math2 = new Math();
+            // Asks for input
+            Console.WriteLine("Enter a number");
+            // Stores input
+            string input2 = Console.ReadLine();
+            // Display result
+            Console.WriteLine(math2.Add(input2));
+
+            Console.ReadLine();
+        }
+    }
+}
