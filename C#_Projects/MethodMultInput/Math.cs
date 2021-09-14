8383bd48c665efcdfd3737400ea81cfb5d3238a5
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-14 15:37:55 -0400

    Add files via upload

---
+++ C#_Projects/MethodMultInput/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMultInput
+{
+    class Math
+    {
+        // If input2 recieves no value, set to default of 0
+        public static int Add(int input, int input2=0)
+        {
+            return (input + input2) * 5;
+        }
+    }
+}
---
+++ C#_Projects/MethodMultInput/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMultInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks for input
+            Console.WriteLine("Enter a number (Required)");
+            // Converts input to an int
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            // Asks for input
+            Console.WriteLine("Enter another number (Optional)");
+            string inputStr = Console.ReadLine();
+            // This checks if the entered value is empty or null
+            if (!String.IsNullOrEmpty(inputStr))
+            {
+                int input2 = Convert.ToInt32(inputStr);
+                Console.WriteLine(Math.Add(input, input2));
+            }
+            // If it is empty/null, only pass the first input
+            else
+            {
+                Console.WriteLine(Math.Add(input));
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
