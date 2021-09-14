03e63e13be5d414f74a88e284317f12d8b2f51f1
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-14 16:05:49 -0400

    Add files via upload

---
+++ C#_Projects/MethodMultInput2/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMultInput2
+{
+    class Math
+    {
+        public void Add(int input, int input2)
+        {
+            Console.WriteLine(input + 5);
+            Console.WriteLine(input2);
+            return;
+        }
+    }
+}
---
+++ C#_Projects/MethodMultInput2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMultInput2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // New instance
+            Math math = new Math();
+            // Asks for input
+            Console.WriteLine("Enter a number");
+            // Stores input
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            // Asks for input
+            Console.WriteLine("Enter another number");
+            // Stores input
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            // Passes input
+            math.Add(input: num1, input2: num2);
+            
+            Console.ReadLine();
+        }
+    }
+}
