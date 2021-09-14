af38051b214b764b2e0a6dda3a81ac2d6cb219a1
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-14 17:15:09 -0400

    Add files via upload

---
+++ C#_Projects/Inheritance/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FName + LName);
+            return;
+        }
+    }
+}
---
+++ C#_Projects/Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating new object
+            Employee employee = new Employee();
+            employee.FName = "Sample";
+            employee.LName = "Student";
+
+            // Calling method that displays object name
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
