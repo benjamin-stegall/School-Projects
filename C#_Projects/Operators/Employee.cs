f157c3d5459fb730c61645e4bb042e4049a27bd5
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-15 14:27:40 -0400

    Add files via upload

---
+++ C#_Projects/Operators/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee
+    {
+        public int ID { get; set; }
+
+        // Overloading ==
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            if (employee1.ID == employee2.ID)
+                return true;
+            else
+                return false;
+        }
+
+        // Overloading !=
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            if (employee1.ID != employee2.ID)
+                return true;
+            else
+                return false;
+        }
+
+        // This allows us to compare the properties as opposed to whole object
+        // By default, c# will return false as seperate objects are different
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        // Also required to avoid errors
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ C#_Projects/Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // New objects
+            Employee employee1 = new Employee();
+            employee1.ID = 2;
+            Employee employee2 = new Employee();
+            employee2.ID = 1;
+
+            // Checks if id is equal
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("They are equal");
+            }
+            else
+            {
+                Console.WriteLine("They are not equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
