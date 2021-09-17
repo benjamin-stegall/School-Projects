31bf45bed82ad76ad60941162f4b7f3976c21275
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-17 12:51:49 -0400

    Add files via upload

---
+++ C#_Projects/Datetime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Displays current time
+            Console.WriteLine("Hello, today it is " + DateTime.Now);
+            // Request user input
+            Console.WriteLine("How many hours would you like to add to the current time?");
+            // Converts user input
+            int input = Convert.ToInt32(Console.ReadLine());
+            // Takes current time and adds designated number of hours
+            DateTime time = DateTime.Now.AddHours(input);
+            // Modified time is displayed
+            Console.WriteLine("The modified time is " + time);
+
+            Console.ReadLine();
+        }
+    }
+}
