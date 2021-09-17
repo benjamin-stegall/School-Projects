2496664414d9921c1f1f7811415eb22cb056c8bd
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-09-17 18:44:54 -0400

    Add files via upload

---
+++ C#_Projects/Constructor/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Person
+    {
+        public string FName { get; set; }
+        public int Age { get; set; }
+
+        // Constructor Chain
+
+        public Person(string input1) : this(input1, 20)
+        {
+        }
+
+        public Person(string input1, int input2)
+        {
+            FName = input1;
+            Age = input2;
+        }
+    }
+}
---
+++ C#_Projects/Constructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using const to store welcome message
+            const string welcome = "Welcome to the Clone Machine";
+            Console.WriteLine(welcome);
+
+            // Storing input as a var
+            var input1 = "John Doe";
+
+            // Display new object
+            Person person = new Person(input1);
+            Console.WriteLine(person.FName + " is " + person.Age + " years old.");
+            Console.ReadLine();
+        }
+    }
+}
