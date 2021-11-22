c36c04a5b3ede36537a8f7039cdb904b1d8485dc
Benjamin Stegall <84801574+benjamin-stegall@users.noreply.github.com>
2021-11-22 14:26:38 -0500

    Add RandomQuotes App

---
+++ C#_Projects/RandomQuotes/RandomQuotes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listOfQuotes = new List<string>()
+            {
+                "“Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).” -Mark Twain",
+                "“The only true wisdom is in knowing you know nothing.” -Socrates",
+                "“Any fool can know. The point is to understand.” -Albert Einstein",
+                "“All knowledge hurts.” -Cassandra Clare"
+            };
+
+            Random rnd = new Random();
+
+            int r = rnd.Next(listOfQuotes.Count);
+
+            Console.WriteLine(listOfQuotes[r]);
+            Console.ReadLine();
+        }
+    }
+}
